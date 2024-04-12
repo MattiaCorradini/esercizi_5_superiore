46dffcb2d5f45cd24d6c133ba7b6d02ad02a6766
Mattia <corradini.mattia05@gmail.com>
2024-04-12 09:19:16 +0200

    Esercizio sul database "world"

--- world/conf/config.php
+++ world/conf/config.php
@@ -1,6 +1,12 @@
 <?php
 
 const DB_HOST = 'localhost';
+const DB_NAME = 'world';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+/*
+
+const DB_HOST = 'localhost';
 const DB_NAME = 'db_corra';
 const DB_USER = 'corra';
-const DB_PASSWORD = '7HF7a2anu4pT';
+const DB_PASSWORD = '7HF7a2anu4pT';*/
