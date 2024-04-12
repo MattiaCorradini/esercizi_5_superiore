1a4091cc4f14f6b456ce4703e0854102c8af9db8
Mattia <corradini.mattia05@gmail.com>
2024-04-12 09:49:09 +0200

    Esempi di autenticazione in php

--- autenticazione/index.php
+++ autenticazione/index.php
@@ -2,8 +2,13 @@
 
     require 'vendor/autoload.php';
     $templates = new League\Plates\Engine('templates', 'tpl');
+    echo $templates -> render('index');
 
+    session_start();
+    if (isset($_SESSION['name'])){
+        echo 'hi ' . $_SESSION['name'];
+    } else {
+        $name = $_GET['name'];
+        $_SESSION['name'] = $name;
+    }
 
-    echo $templates -> render('index', []);
-
-?>
